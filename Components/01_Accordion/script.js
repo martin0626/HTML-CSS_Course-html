9652b2cae96c64e20def9d84d9ab143ec233dc27
martin0626 <scbsteel12@gmail.com>
2022-09-15 20:36:15 +0300

    Carousel elements and Accordion finish

--- Components/01_Accordion/script.js
+++ Components/01_Accordion/script.js
@@ -1,12 +1,16 @@
 function openAcc() {
-  let button = document.getElementById("try");
-  button.addEventListener("click", () => {
-    if (Array.from(button.classList).includes("active")) {
-      button.className = "item";
-    } else {
-      button.className += " active";
-    }
-  });
+  let buttonElemetnts = Array.from(document.getElementsByClassName("item"));
+
+  for (let i = 0; i < buttonElemetnts.length; i++) {
+    let button = buttonElemetnts[i];
+    button.addEventListener("click", () => {
+      if (Array.from(button.classList).includes("active")) {
+        button.className = "item";
+      } else {
+        button.className += " active";
+      }
+    });
+  }
 }
 
 console.log("asd");
