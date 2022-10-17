2c6764c1cb4c89ae39eec04bdf204801ee7ce0e5
martin0626 <scbsteel12@gmail.com>
2022-10-17 21:15:37 +0300

    Added sticky navigation, favicon, meata description and Safari Grid property fix

--- FinalProjectHTML&CSS/js/script.js
+++ FinalProjectHTML&CSS/js/script.js
@@ -3,4 +3,29 @@ function changeYear() {
   yearElement.textContent = new Date().getFullYear();
 }
 
+function stickyNavigation() {
+  let sectionHeroElement = document.querySelector(".hero-section");
+  let obs = new IntersectionObserver(
+    function (entries) {
+      let ent = entries[0];
+
+      if (!ent.isIntersecting) {
+        document.body.classList.add("sticky");
+      }
+
+      if (ent.isIntersecting) {
+        document.body.classList.remove("sticky");
+      }
+    },
+    {
+      root: null,
+      threshold: 0,
+      rootMargin: "-80px",
+    }
+  );
+
+  obs.observe(sectionHeroElement);
+}
+
+stickyNavigation();
 changeYear();
